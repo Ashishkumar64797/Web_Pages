e9d1fcc561aaabad93e7e6cde5f70fbeff20c061
Ashishkumar64797 <115533866+Ashishkumar64797@users.noreply.github.com>
2023-01-09 21:05:20 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,35 @@
+(function(){
+
+    let screen = document.querySelector('.screen')
+    let buttons = document.querySelectorAll('.btn')
+    let clear = document.querySelector('.btn-clear')
+    let equal = document.querySelector('.btn-equal')
+    let cut = document.querySelector('.btn-cut')
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    })
+
+    equal.addEventListener('click',function(e){
+        if(screen.value === ''){
+            screen.value = '';
+        }
+        else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    })
+
+    clear.addEventListener('click',function(e){
+        screen.value=""
+    })
+
+    /*cut.addEventListener('click',function(e){
+        let temp=screen.value
+        screen.value=temp.slice(0,-1)
+    })*/
+
+})();
